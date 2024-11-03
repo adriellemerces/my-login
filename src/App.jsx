9cda9660d8274c3ca4ee2e4b45ca2f5e598e47e0
Adrielle Merces <adriellemerces75@gmail.com>
2024-11-03 10:54:26 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Acesso from './Components/Login/Acesso';
+
+function App() {
+  
+
+  return (
+   <div className="app">
+  
+    <Acesso/>
+    
+   </div>
+  );
+}
+
+export default App;
